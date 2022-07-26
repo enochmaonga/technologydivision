350560bb3b642351a44fb4178b1e7223cdc3c96b
enoch.maonga <enoch.maonga@gmail.com>
2022-07-26 18:52:34 +0300

    adding insertions to Sql2odptmntsDao

--- src/main/java/dao/Sql2oDepartmentsDao.java
+++ src/main/java/dao/Sql2oDepartmentsDao.java
@@ -6,15 +6,114 @@ import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
 
 
+import java.sql.Connection;
 import java.util.List;
+import java.util.Objects;
 
-public class Sql2oDepartmentsDao implements DepartmentsDao{
+public class Sql2oDepartmentsDao implements DepartmentsDao {
     private final Sql2o Sql2o;
-    public Sql2oDepartmentsDao(Sql2o sql2o){
+
+    public Sql2oDepartmentsDao(Sql2o sql2o) {
         this.sql2o = sql2o;
     }
 
     @Override
-    List<Departments> getAll() {
-        return super.getAll();
+    public List<Departments> getAll() {
+        try (Connection con = sql2o.open()) {
+            return con.creatQuery("SELECT * FROM departments")
+                    .executeAndFetch(departements.class);
+        }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Sql2oDepartmentsDao that = (Sql2oDepartmentsDao) o;
+//        return Objects.equals(Sql2o, that.Sql2o);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(Sql2o);
+//    }
+//
+//    public Sql2o getSql2o() {
+//        return Sql2o;
+//    }
+//
+//    public Sql2o getSql2o() {
+//        return Sql2o;
+//    }
+//
+//    @Override
+//    Departments findById() {
+//        return super.findById();
+//    }
+
+        @Override
+        public void add (Departments departments){
+            String sql = "INSERT INTO departments (name) VALUES (:name)";
+            try (Connection con = sql2o.open()) {
+                int id = (int) con.createQuery(sql, true)
+                        .bind(departments)
+                        .executeUpdate()
+                        .getKey();
+                departments.setId(id);
+            } catch (Sql2oException ex) {
+                System.out.println(ex);
+            }
+        }
+
+
+        @Override
+        public Departments findById ( int id){
+            try (Connection con = sql2o.open()) {
+                return con.createQuery("SELECT * FROM departments WHERE id = :id")
+                        .addParameter("id", id)
+                        .executeAndFetchFirst(Departments.class);
+            }
+        }
+
+
+        @Override
+        public void update ( int id, String name){
+            String sql = "UPDATE departments SET name = :name WHERE id=:id";
+            try (Connection con = sql2o.open()) {
+                con.createQuery(sql)
+                        .addParameter("name", name)
+                        .addParameter("id", id)
+                        .executeUpdate();
+            } catch (Sql2oException ex) {
+                System.out.println(ex);
+            }
+        }
+        ;
+
+
+        @Override
+        public void deleteById ( int id){
+            String sql = "DELETE from departments WHERE id=:id";
+            try (Connection con = sql2o.open()) {
+                con.createQuery(sql)
+                        .addParameter("id", id)
+                        .executeUpdate();
+            } catch (Sql2oException ex) {
+                System.out.println(ex);
+            }
+        }
+
+
+        @Override
+        public void deleteAllDepartments () {
+            String sql = "DELETE from departments";
+            try (Connection con = sql2o.open()) {
+                con.createQuery(sql)
+                        .executeUpdate();
+            } catch (Sql2oException ex) {
+                System.out.println(ex);
+            }
+        }
+
+
     }
+}
