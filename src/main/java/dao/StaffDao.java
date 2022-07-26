e88bddcab4ef4850ca5b90832ed5c317e26adf45
enoch.maonga <enoch.maonga@gmail.com>
2022-07-26 23:06:29 +0300

    added staffDao andsqlsaff Dao

---
+++ src/main/java/dao/StaffDao.java
@@ -0,0 +1,35 @@
+package dao;
+import models.Staff;
+
+
+import java.util.List;
+
+public interface StaffDao {
+
+
+
+
+    // LIST
+    List<Staff> getAll();
+    List<Staff> getByDepartment(int departmentId);
+
+
+    // CREATE
+    void add(Staff staff);
+
+
+    // READ
+    Staff findById(int id);
+
+
+    // UPDATE
+    void update(int id, String content);
+
+
+    // DELETE
+    void deleteById(int id);
+    void deleteAllStaff();
+
+
+}
+
