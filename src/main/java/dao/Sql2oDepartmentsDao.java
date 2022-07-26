fa6163e7cb15075858b610d60382415b448e2221
enoch.maonga <enoch.maonga@gmail.com>
2022-07-26 18:34:01 +0300

    adding src/main/java/dao/Sql2oDepartmentsDao.java

---
+++ src/main/java/dao/Sql2oDepartmentsDao.java
@@ -0,0 +1,20 @@
+package dao;
+import models.Departments;
+import models.Staff;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+
+import java.util.List;
+
+public class Sql2oDepartmentsDao implements DepartmentsDao{
+    private final Sql2o Sql2o;
+    public Sql2oDepartmentsDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    List<Departments> getAll() {
+        return super.getAll();
+    }
